9e2005a49634cecc9708260891d2fe1091fd14a4
Dmitriy <mr.d.fagot@gmail.com>
2019-03-30 00:46:39 +0300

    Add solutions for part 2

--- part_1/letter_pos.py
+++ part_1/letter_pos.py
@@ -5,4 +5,5 @@ st = set()
 for ch in s_s:
     if ch.isalpha() and ch not in st:
         st.add(ch)
-        print(ch, ' '.join(str(i+1) for i, v in enumerate(f_s) if v == ch) or 'None')
+        print(ch, ' '.join(str(i + 1)
+                           for i, v in enumerate(f_s) if v == ch) or 'None')
---
+++ part_2/tmp.py
@@ -0,0 +1,16 @@
+list_a = [1, 2, 3, 4]
+
+list_b = [1, ]
+
+list_c = []
+
+list_d = [[]]
+
+list_e = list()
+
+print(list_c)
+print(list_d)
+print(list_e)
+print(list_a * 0)
+print(list_b.pop())
+print(list_a[-1:0])
